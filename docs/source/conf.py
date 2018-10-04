1bf8aea8bf2c1841dab41d562295816274f75ad0
Alessio Buccino <alejoe9187@gmail.com>
2018-10-04 11:38:40 +0200

    added mathjax extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,9 +38,11 @@ release = '1.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.mathjax']
-
-mathjax_path = '/home/alessio/Documents/Codes/MathJax/MathJax.js'
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.doctest',
+              'sphinx.ext.autosummary',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
