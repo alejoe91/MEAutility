fcd3fda802e4f36e81947a7d4df449dcea49d8cc
Alessio Buccino <alejoe9187@gmail.com>
2019-05-19 13:34:03 +0200

    fix tests

--- MEAutility/tests/test_plotting.py
+++ MEAutility/tests/test_plotting.py
@@ -2,28 +2,34 @@ import pytest
 import numpy as np
 import MEAutility as mu
 
+
 def test_plot_probe():
     mea = mu.return_mea('Neuronexus-32')
     ax = mu.plot_probe(mea)
 
+
 def test_plot_probe_3d():
     mea = mu.return_mea('Neuronexus-32')
     ax = mu.plot_probe_3d(mea)
-    mea = mu.return_mea('SqMEA-10-15um')
+    mea = mu.return_mea('SqMEA-10-15')
     ax = mu.plot_probe_3d(mea, ax=ax, type='planar', color_currents=True)
+
 
 def test_plot_v_image():
     mea = mu.return_mea('Neuronexus-32')
     ax = mu.plot_v_image(mea, offset=30, y_bound=[-100, 100], z_bound=[-100, 100])
 
+
 def test_plot_v_surf():
     mea = mu.return_mea('Neuronexus-32')
     ax = mu.plot_v_surf(mea, offset=30, y_bound=[-100, 100], z_bound=[-100, 100])
+
 
 def test_plot_mea_recording():
     mea = mu.return_mea('Neuronexus-32')
     rec = np.random.randn(32, 500)
     ax = mu.plot_mea_recording(rec, mea)
+
 
 def test_play_mea_recording():
     mea = mu.return_mea('Neuronexus-32')
