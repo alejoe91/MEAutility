cf854474065644b5fd47a9dd7e430d8984960d91
Alessio Buccino <alejoe9187@gmail.com>
2025-01-14 13:21:47 +0100

    Add __version__

--- src/MEAutility/__init__.py
+++ src/MEAutility/__init__.py
@@ -1,2 +1,6 @@
+import importlib.metadata
+
+__version__ = importlib.metadata.version("MEAutility")
+
 from .core import MEA, RectMEA, Electrode, return_mea, return_mea_info, return_mea_list, add_mea, remove_mea
 from .plotting import *
